525ceb2796fc9db01cecc74bc14f25bcfe22fbe3
altafshaikh <iamaltafshaikh07@gmail.com>
2020-12-21 13:31:58 +0530

    created class component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Heading from "./components/Heading";
+import Paragraph from "./components/Paragraph";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Heading />
+        <Paragraph />
       </header>
     </div>
   );
---
+++ src/components/Paragraph.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+// creata a class component
+
+class Paragraph extends React.Component {
+  render() {
+    return <p>Paragraph using class component</p>;
+  }
+}
+
+export default Paragraph;
